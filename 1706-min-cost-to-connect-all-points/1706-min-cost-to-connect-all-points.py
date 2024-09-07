d9a473f28b55902998e60c018db017c9be4d01c6
StafShah <110946647+StafShah@users.noreply.github.com>
2024-09-07 15:36:02 -0400

    Time: 1118 ms (65.75%), Space: 100.8 MB (54.1%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -1,10 +1,10 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         visited = set()
-        minH = [(0, (points[0][0], points[0][1]))]
         res = 0
+        minH = [(0, (points[0][0], points[0][1]))]
 
-        while minH:
+        while len(visited) < len(points):
             dist, coordinates = heapq.heappop(minH)
             if coordinates in visited:
                 continue
@@ -13,7 +13,7 @@ class Solution:
 
             for x, y in points:
                 if (x, y) not in visited:
-                    dist1 = abs(coordinates[0] - x) + abs(coordinates[1] - y)
-                    heapq.heappush(minH, (dist1, (x, y)))
-            
+                    newDist = abs(coordinates[0] - x) + abs(coordinates[1] - y)
+                    heapq.heappush(minH, (newDist, (x, y)))
+        
         return res
